7d9677f1fe4572515946d5c72ffc1570880b8125
pauloalvesm <paulo.alves7351@gmail.com>
2026-05-17 12:00:50 -0300

    feat: add console application project

---
+++ src/DesignPatterns.Course/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Design Patterns Course **********");
+
+        Console.ReadKey();
+    }
+}
