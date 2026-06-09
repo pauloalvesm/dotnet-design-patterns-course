7167f65a6d2af30156bb081d4e83952ad2eacc59
pauloalvesm <paulo.alves7351@gmail.com>
2026-06-09 19:33:25 -0300

    feat: create an example for the mediator pattern

---
+++ src/DesignPatterns.Course/03-Behavioral/09-Mediator/AbstractModel/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Course._03_Behavioral._09_Mediator.AbstractModel;
+
+public abstract class Contact
+{
+    protected Mediator Mediator;
+
+    protected Contact(Mediator mediator)
+    {
+        Mediator = mediator;
+    }
+}
---
+++ src/DesignPatterns.Course/03-Behavioral/09-Mediator/ConcreteMediator.cs
@@ -0,0 +1,37 @@
+﻿using DesignPatterns.Course._03_Behavioral._09_Mediator.AbstractModel;
+
+namespace DesignPatterns.Course._03_Behavioral._09_Mediator;
+
+public class ConcreteMediator : Mediator
+{
+    private Attendant _attendant;
+    private Patient _patient;
+
+    public Attendant Attendant
+    {
+        set
+        {
+            _attendant = value;
+        }
+    }
+
+    public Patient Patient
+    {
+        set
+        {
+            _patient = value;
+        }
+    }
+
+    public override void Send(string message, Contact contact)
+    {
+        if (contact == _patient)
+        {
+            _attendant.Notify(message);
+        }
+        else
+        {
+            _patient.Notify(message);
+        }
+    }
+}
